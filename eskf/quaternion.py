bec8cd9570c1ec7633afadc7381c9288d4b5affa
Henrik Storli <henrisst@stud.ntnu.no>
2021-10-25 13:00:22 +0200

    Task 2 done

--- eskf/quaternion.py
+++ eskf/quaternion.py
@@ -1,11 +1,14 @@
 import numpy as np
 from numpy import ndarray
 from dataclasses import dataclass
+from numpy.lib.type_check import real
 from scipy.spatial.transform import Rotation
 
 from config import DEBUG
 
 import solution
+
+from cross_matrix import get_cross_matrix
 
 
 @dataclass
@@ -51,17 +54,20 @@ class RotationQuaterion:
         """
 
         # TODO replace this with your own code
-        quaternion_product = solution.quaternion.RotationQuaterion.multiply(
-            self, other)
+        real_part = self.real_part*other.real_part - self.vec_part.T@other.vec_part
+        vec_part = other.real_part*self.vec_part + self.real_part*other.vec_part + get_cross_matrix(self.vec_part)@other.vec_part
+
+        quaternion_product = RotationQuaterion(real_part,vec_part)
 
         return quaternion_product
 
     def conjugate(self) -> 'RotationQuaterion':
         """Get the conjugate of the RotationQuaternion"""
 
-        # TODO replace this with your own code
-        conj = solution.quaternion.RotationQuaterion.conjugate(self)
 
+        # # TODO replace this with your own code
+        # conj = solution.quaternion.RotationQuaterion.conjugate(self)
+        conj = RotationQuaterion(self.real_part,-self.vec_part)
         return conj
 
     def as_rotmat(self) -> 'ndarray[3,3]':
@@ -72,7 +78,9 @@ class RotationQuaterion:
         """
 
         # TODO replace this with your own code
-        R = solution.quaternion.RotationQuaterion.as_rotmat(self)
+        #R = solution.quaternion.RotationQuaterion.as_rotmat(self)
+
+        R = np.eye(3) + 2*self.real_part*get_cross_matrix(self.vec_part) + 2*get_cross_matrix(self.vec_part)@get_cross_matrix(self.vec_part)
 
         return R
 
@@ -88,7 +96,16 @@ class RotationQuaterion:
         """
 
         # TODO replace this with your own code
-        euler = solution.quaternion.RotationQuaterion.as_euler(self)
+        #euler = solution.quaternion.RotationQuaterion.as_euler(self)
+
+        real_part = self.real_part
+        vec_part = self.vec_part
+
+        phi = np.arctan2(2*(vec_part[2]*vec_part[1] + real_part*vec_part[0]), real_part**2 - vec_part[0]**2 - vec_part[1]**2 + vec_part[2]**2)
+        theta = np.arcsin(2*(real_part*vec_part[1] - vec_part[0]*vec_part[2]))
+        psi = np.arctan2(2*(vec_part[0]*vec_part[1] + real_part*vec_part[2]), real_part**2 + vec_part[0]**2 - vec_part[1]**2-vec_part[2]**2)
+
+        euler = np.array([phi, theta, psi])
 
         return euler
 
@@ -100,7 +117,12 @@ class RotationQuaterion:
         """
 
         # TODO replace this with your own code
-        avec = solution.quaternion.RotationQuaterion.as_avec(self)
+        #avec = solution.quaternion.RotationQuaterion.as_avec(self)
+
+        alpha = np.arccos(self.real_part)*2
+        n = self.vec_part/np.sin(alpha/2)
+        avec = n*alpha
+
 
         return avec
 
